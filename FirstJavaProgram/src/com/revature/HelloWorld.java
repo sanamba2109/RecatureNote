71bc6254ccf04b6aabd12f0a417edbf8fca2b54d
camaraT <tsctrefilo@gmail.com>
2021-08-23 14:32:46 -0500

    add hello world class

---
+++ FirstJavaProgram/src/com/revature/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+public class HelloWorld {
+/**
+ * this is java documentation
+ * @param args
+ */
+	public static void main(String[] args) {
+		//single line comment 
+		/* this is a multi line comment*/
+		System.out.println("this is my new World from the ADE");
+		// TODO Auto-generated method stub
+
+	}
+
+}
